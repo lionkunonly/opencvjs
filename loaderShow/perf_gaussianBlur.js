d153f4ddde4c08e39c1a7b64bc44ce7dc082464b
lionkun <871518554@qq.com>
2020-08-31 13:04:59 +0800

    change the loader example from blur to gaussianBlur

--- loaderShow/perf_gaussianBlur.js
+++ loaderShow/perf_gaussianBlur.js
@@ -25,29 +25,27 @@ function perf() {
     }
     let totalCaseNum, currentCaseId;
 
-    const BlurSize = [cvSize.szODD, cvSize.szQVGA, cvSize.szVGA, cvSize.sz720p];
-    const Blur5x16Size = [cvSize.szVGA, cvSize.sz720p];
-    const BlurType = ["CV_8UC1", "CV_8UC4", "CV_16UC1", "CV_16SC1", "CV_32FC1"];
-    const BlurType5x5 = ["CV_8UC1", "CV_8UC4", "CV_16UC1", "CV_16SC1", "CV_32FC1", "CV_32FC3"];
+    const GaussianBlurSize = [cvSize.szODD, cvSize.szQVGA, cvSize.szVGA, cvSize.sz720p];
+    const GaussianBlurType = ["CV_8UC1", "CV_8UC4", "CV_16UC1", "CV_16SC1", "CV_32FC1"];
     const BorderType3x3 = ["BORDER_REPLICATE", "BORDER_CONSTANT"];
-    const BorderTypeAll = ["BORDER_REPLICATE", "BORDER_CONSTANT", "BORDER_REFLECT", "BORDER_REFLECT101"];
+    const BorderType3x3ROI = ["BORDER_REPLICATE", "BORDER_CONSTANT", "BORDER_REFLECT", "BORDER_REFLECT101"];
 
-    const combiBlur3x3 = combine(BlurSize, BlurType, BorderType3x3);
-    const combiBlur16x16 = combine(Blur5x16Size, BlurType, BorderTypeAll);
-    const combiBlur5x5 = combine(Blur5x16Size, BlurType5x5, BorderTypeAll);
+    const combiGaussianBlurBorder3x3 = combine(GaussianBlurSize, GaussianBlurType, BorderType3x3);
+    const combiGaussianBlurBorder3x3ROI = combine(GaussianBlurSize, GaussianBlurType, BorderType3x3ROI);
 
-    function addBlurCase(suite, type) {
-        suite.add('blur', function() {
-            cv.blur(src, dst, ksize, new cv.Point(-1,-1), borderType);
+    function addGaussianBlurCase(suite, type) {
+        suite.add('gaussianBlur', function() {
+            cv.GaussianBlur(src, dst, ksize, 1, 0, borderType);
           }, {
               'setup': function() {
                 let size = this.params.size;
                 let matType = cv[this.params.matType];
                 let borderType = cv[this.params.borderType];
-                let ksizeNum = this.params.ksize;
-                let ksize = new cv.Size(ksizeNum, ksizeNum);
+                let type = this.params.type;
                 let src = new cv.Mat(size, matType);
                 let dst = new cv.Mat(size, matType);
+                let ksizeNum = this.params.ksize;
+                let ksize = new cv.Size(ksizeNum, ksizeNum);
                 },
               'teardown': function() {
                 src.delete();
@@ -56,16 +54,15 @@ function perf() {
           });
     }
 
-    function addBlurModeCase(suite, combination, type) {
+    function addGaussianBlurModeCase(suite, combination, type) {
       totalCaseNum += combination.length;
       for (let i = 0; i < combination.length; ++i) {
         let size =  combination[i][0];
         let matType = combination[i][1];
         let borderType = combination[i][2];
-        let ksizeArray = [3, 16, 5];
-
+        let ksizeArray = [3, 5];
         let params = {size: size, matType:matType, ksize: ksizeArray[type], borderType:borderType};
-        addKernelCase(suite, params, type, addBlurCase);
+        addKernelCase(suite, params, type, addGaussianBlurCase);
       }
     }
 
@@ -80,25 +77,24 @@ function perf() {
           paramObjs.push({name:"size", value:"", reg:[""], index:0});
           paramObjs.push({name:"matType", value:"", reg:["/CV\_[0-9]+[FSUfsu]C[0-9]/"], index:1});
           paramObjs.push({name:"borderMode", value: "", reg:["/BORDER\_\\w+/"], index:2});
-          let locationList = decodeParams2Case(params, paramObjs,blurCombinations);
+          let locationList = decodeParams2Case(params, paramObjs,gaussianBlurCombinations);
 
           for (let i = 0; i < locationList.length; i++){
               let first = locationList[i][0];
               let second = locationList[i][1];
-              addBlurModeCase(suite, [blurCombinations[first][second]], first);
+              addGaussianBlurModeCase(suite, [gaussianBlurCombinations[first][second]], first);
             }
       } else {
         log("no filter or getting invalid params, run all the cases");
-        addBlurModeCase(suite, combiBlur3x3, 0);
-        addBlurModeCase(suite, combiBlur16x16, 1);
-        addBlurModeCase(suite, combiBlur5x5, 2);
+        addGaussianBlurModeCase(suite, combiGaussianBlurBorder3x3, 0);
+        addGaussianBlurModeCase(suite, combiGaussianBlurBorder3x3ROI, 1);
       }
-      setBenchmarkSuite(suite, "blur", currentCaseId);
-      log(`Running ${totalCaseNum} tests from blur`);
+      setBenchmarkSuite(suite, "gaussianBlur", currentCaseId);
+      log(`Running ${totalCaseNum} tests from gaussianBlur`);
       suite.run({ 'async': true }); // run the benchmark
   }
 
-    let blurCombinations = [combiBlur3x3, combiBlur16x16, combiBlur5x5];
+    let gaussianBlurCombinations = [combiGaussianBlurBorder3x3, combiGaussianBlurBorder3x3ROI];
 
     if (isNodeJs) {
       const args = process.argv.slice(2);
